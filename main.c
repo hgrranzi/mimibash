4785df5e262b605d43dce0535a24aa6c9e02f423
hgrranzi <vkh.mariia@gmail.com>
2021-07-15 08:25:39 +0300

    Moved handle_signal from the loop

--- main.c
+++ main.c
@@ -4,10 +4,10 @@ int	main(int argc, char **argv)
 {
 	t_data	*head_data;
 
+	//handle_signal();
 	head_data = init_data();
 	while (1)
 	{
-		//handle_signal();
 		//fill_history();
 		//parse_data();
 		//exec_cmd();
