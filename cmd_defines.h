a6685391daa74dd73b5f1d4daaeaed1e2a9efdfe
hgrranzi <vkh.mariia@gmail.com>
2021-07-16 07:53:01 +0300

    Defined builtins

--- cmd_defines.h
+++ cmd_defines.h
@@ -1,6 +1,12 @@
 #ifndef CMD_DEFINES_H
 # define CMD_DEFINES_H
 
-# define PWD 1
+# define ECHO 1
+# define CD 2
+# define PWD 3
+# define EXPORT 4
+# define UNSET 5
+# define ENV 6
+# define EXIT 7
 
 #endif
