57eaca2bd03564e4291cd40bff70a6fa92999404
hgrranzi <vkh.mariia@gmail.com>
2021-08-03 19:23:00 +0300

    Started working with argv[1] of the programm

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int	main(int argc, char **argv, char **envp)
 	int		(*builtins[8])(int *, char **, char ***);
 	char	**envp_copy;
 	int		exit_status;
+	int		fd_in;
 
 	handle_signal();
 	init_builtins(builtins);
@@ -14,6 +15,13 @@ int	main(int argc, char **argv, char **envp)
 	init_shlvl(&envp_copy);
 	head_data = NULL;
 	exit_status = 0;
+	fd_in = STDIN_FILENO;
+	if (argc > 1)
+	{
+		fd_in = open(argv[1], O_RDONLY, 0644);
+		if (fd_in == -1)
+			error_and_exit(argv[1], NULL, 1);
+	}
 	//head_data = init_data(); // no need probably
 	while (1)
 	{
@@ -21,6 +29,8 @@ int	main(int argc, char **argv, char **envp)
 		// printf("%s\n", input);
 		// printf ("%d", head_data->builtin);
 		// read (0, input, 10);
+		if (fd_in != STDIN_FILENO)
+			dup2(fd_in, STDIN_FILENO);
 		input = readline (PROMPT);
 		if (!input) // or "exit"
 		{
@@ -34,6 +44,8 @@ int	main(int argc, char **argv, char **envp)
 
 		}
 		// printf("%s %s %s\n", head_data->args[0], head_data->args[1], head_data->args[2]);
+		if (fd_in != STDIN_FILENO)
+			dup2(STDIN_FILENO, fd_in);
 		exit_status = exec_cmd(&head_data, builtins, &envp_copy);
 		free(input);
 		// exit (0);
